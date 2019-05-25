293316ae42ee9cf8e48a0791d4884eb1aa01bdb0
starbuck <mausuf@utexas.edu>
2019-05-25 00:13:16 -0500

    get current user's profile user, name, and avatar. Tested in Postman: 'Get logged in users profile'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,10 +3,29 @@
 // Bring in Express router
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public (Does not require token to access, otherwise user will get unauthorized access message)
-router.get("/", (req, res) => res.send("Profile route"));
+//Bring in Profile & User models
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   GET api/profile/me --> route to just get ours based on the user id that's in the token
+// @desc    Get current user's profile
+// @access  Private (Use auth middleware)
+router.get("/me", auth, async (req, res) => { // Added auth as second parameter to pass in auth middleware
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate(["user", "name", "avatar"]); // Found in Profile.js model -> user field's ObjectID; set req.user.id to the "user:" that comes in. use method 'populate' from USER model and bring in name and avatar since currently were pulling from PROFILE model.
+
+        if(!profile) { //If no profile exists
+            return res.status(400).json({ msg: "No profile exist for this user" })
+        }
+        
+        res.json(profile); //If there is a profile
+
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send("Server error :(");
+    }
+}); 
 
 module.exports = router;
