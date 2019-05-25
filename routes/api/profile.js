f0f9bfd8468b2bd9926eff8591d3697b052d6187
starbuck <mausuf@utexas.edu>
2019-05-25 13:09:09 -0500

    updated profile route with POST with 2 auth middlewares and tested with Postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check"); //Documentation @: https://express-validator.github.io/docs/
 
 //Bring in Profile & User models
 const Profile = require("../../models/Profile");
@@ -19,7 +20,7 @@ router.get("/me", auth, async (req, res) => { // Added auth as second parameter 
         if(!profile) { //If no profile exists
             return res.status(400).json({ msg: "No profile exist for this user" })
         }
-        
+
         res.json(profile); //If there is a profile
 
     } catch(err){
@@ -27,5 +28,24 @@ router.get("/me", auth, async (req, res) => { // Added auth as second parameter 
         res.status(500).send("Server error :(");
     }
 }); 
+
+// @route   POST api/profile/
+// @desc    Create or update user profile
+// @access  Private (Use auth middleware & validation middleware)
+router.post("/", [ auth, [   // 1) First middleware is 'auth' 2) second middleware is containtained in brackets which are the 2 'check'
+    check("status", "Status is required").not().isEmpty(),
+    check("skills", "Skills is required").not().isEmpty()
+    ]
+],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) { // If there are errors then we want to return status of 400
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        
+    }
+);
+
 
 module.exports = router;
