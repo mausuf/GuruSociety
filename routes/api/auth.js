39e720e1385cdaa07b82bc5bbfcb20798413ec0f
starbuck <mausuf@utexas.edu>
2019-05-24 22:43:44 -0500

    updated middleware user routes res user data from mongoDB database instead of message 'auth user', tested in Postman

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,8 +5,18 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
+const User = require("../../models/User")
+
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public (Does not require token to access, otherwise user will get unauthorized access message)
-router.get("/", auth, (req, res) => res.send("Auth route")); // Added auth as a second paramenter to use the middleware. Just doing this makes this route protected
+router.get("/", auth, async (req, res) => { // Added auth as a second paramenter to use the middleware. Just doing this makes this route protected; added function async. Function to make call to database.
+    try{
+        const user = await User.findById(req.user.id).select("-password"); //req.user is pulled from middleware -> auth.js; -password leaves off password in res
+        res.json(user);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send("Sever error :(");
+    }
+}); 
 module.exports = router;
