2d0f9eb4cfa3f4b69d4bcadbbe1163f39c64effc
starbuck <mausuf@utexas.edu>
2019-05-25 22:22:42 -0500

    connected to Github api and successfully fetch github repos for profile based on Github username; tested in Postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,10 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator/check"); //Documentation @: https://express-validator.github.io/docs/
 
+//Below two packages needed for uri*
+const request = require("request");
+const config = require("config");
+
 //Bring in Profile & User models
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -319,5 +323,33 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     }
 });
 
+
+// @route   GET api/profile/education/:username
+// @desc    Get user repos from Github
+// @access  Public --> Since anyone can view a profile to show repositories
+router.get("/github/:username", (req, res) => {
+    try {
+
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`,// 5 repos per page sort by dated created in ascending order, get client id and client secret from config using config npm package
+            method: "GET",
+            headers: { "user-agent": "node.js" }  // user-agent needed for this to work
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200) {  // 200=successful - If no github username found
+                return res.status(404).json({ msg: "No Github profile found" });
+            }
+
+            res.json(JSON.parse(body)); // The body will originally be a regular string with escaped quotes, so need to surround with JSON.parse
+        });
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send("Server error :/");
+    }
+})
 
 module.exports = router;
