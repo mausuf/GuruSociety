5f7e2ce699bbcd18910a08b1b405417a72c19c80
starbuck <mausuf@utexas.edu>
2019-05-25 22:40:26 -0500

    created the Post Model (comments, likes, message posts)

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // Create variable called Schema
+
+const PostSchema = new Schema ({
+    user: { // Post need to be connected to a user so reference to user
+        type: Schema.type.ObjectId, 
+        ref: 'users' // reference users model
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: { // If a user deletes their profile, we want their name and avatar to still remain on the posts made by that now deleted user & profile*
+        type: String
+    },
+    avatar: { //*reference above
+        type: String
+    },
+    likes: [ // Create an array of likes; app has ability to like posts
+        {
+            user: { // This is to know which likes from which user, and also to limit each user to like each post only once
+                type: Schema.Types.ObjectId, 
+                ref: "users"
+            }
+        }
+    ],
+    comments: [
+        {
+            user: { // This is to know which comments from which user
+                type: Schema.Types.ObjectId, 
+                ref: "users"
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+});
+
+
+module.exports = Post = mongoose.model("post", PostSchema);
