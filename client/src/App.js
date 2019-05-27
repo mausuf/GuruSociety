af914a836118aedb10ae5e8599b56359fded36a2
starbuck <mausuf@utexas.edu>
2019-05-27 13:20:52 -0500

    created Landing & Navbar components; integrated FontAwesome; integrated first interation CSS template; integrated components into App.js; app running

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react'; // Fragment is a ghost element that doesn't show up in dom
 import './App.css';
 
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing"
+
+
 
 const App = () => // no need for curly brace because we're returning a single div or fragment
   <Fragment>
-    <h1>App WIP</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 
 
