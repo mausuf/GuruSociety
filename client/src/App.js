1a24d62941e9b53245a2002a1da799e0f47aa6e6
starbuck <mausuf@utexas.edu>
2019-05-27 14:00:50 -0500

    created auth componenets Login & Register, integrated into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Require to import react router since currently landing page is embedded in the App component
 
@@ -14,6 +16,12 @@ const App = () => (
   <Fragment>
     <Navbar />
     <Route exact path="/" component={Landing} />    {/* '/' is the index page, load component Landing  */}
+    <section className="container">                 {/* Need class of container to push everything to middle, however landing page doesn't have container since background image needs to be full screen  */}
+    <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+    </Switch>
+    </section>
   </Fragment>
 </Router>
 );
