cd946da2faee300a11983a4be255ef1894c917aa
starbuck <mausuf@utexas.edu>
2019-05-28 14:14:27 -0500

    updated ROOT REDUCER with AUTH in order to see STATE in REDUX DEV TOOLS; User is able to successfully register and data pushed to monboDB in Previous Commit

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,15 @@
 // Root Reducer
+// File used for REDUX DEV TOOLS
 
 import { combineReducers } from "redux";
 
 // Bring in alert reducer
 import alert from "./alert";
 
+//Bring in auth reducer so we cna see the State of user registration within REDUX DEV TOOLS
+import auth from "./auth";
+
 export default combineReducers({
-    alert
+    alert,
+    auth
 });
