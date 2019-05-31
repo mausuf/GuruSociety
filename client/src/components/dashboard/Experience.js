b54987ddcc9fe920c53bbb6f4ae4ef7a847602cf
starbuck <mausuf@utexas.edu>
2019-05-31 16:50:46 -0500

    created Experience & Education Component to display both on profile; initialized Experience Component starting point

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,17 @@
+// Initialized with ES7 [ racfp ]
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Experience = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Experience.propTypes = {
+
+}
+
+export default Experience
