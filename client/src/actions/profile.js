0e4cff2985bdfcda00dcb7bc06b76ff3e378d30b
starbuck <mausuf@utexas.edu>
2019-05-31 19:16:17 -0500

    created DELETE ACTION for Experience & Education in profile.js

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -125,3 +125,41 @@ export const addEducation = (formData, history) => async dispatch => {     // ta
         });
     }
 };
+
+// Delete Experience ---> route found in: routes->api->profile.js
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`); 
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert("Experience Removed", "danger"))
+    } catch(err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Delete Education ---> route found in: routes->api->profile.js
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`); 
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert("Education Removed", "danger"))
+    } catch(err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
