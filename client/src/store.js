b74c6142d086ad66b1e01f94ccd51c16d862bb8e
starbuck <mausuf@utexas.edu>
2019-06-01 05:58:38 -0500

    HerokuTesting3

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 // Boiler plate code below ---> in order to run Redux Devtools otherwise error in redux devtools: "No store found. Make sure to follow the instructions."
 
-import { createStore, applyMiddleware }from "redux"; // applyMiddleware is used for Thunk
+import { createStore, applyMiddleware, compose }from "redux"; // applyMiddleware is used for Thunk; added compose for Heroku testing
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk"; // thunk is middleware
 import  rootReducer from "./reducers"; // Combining all reducers into root reducer; We will have multiple reducers e.g. auth, profile, alert; from file called 
@@ -9,7 +9,19 @@ const initialState = {}; // initial state is an empty object; all initialStates 
 
 const middleware = [thunk];
 
-// Create the store
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+// Create the store - commented out for Heroku testing
+// const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+// ----Heroku Testing----
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+    //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+// ----------------------
 
 export default store;
