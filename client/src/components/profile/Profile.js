5133a359053ed2c22d20000260cb13b82fc5b8f9
starbuck <mausuf@utexas.edu>
2019-07-12 19:43:35 -0500

    Created user's Profile component and Initial Setup

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,28 @@
+// Initialized with racfp
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfileById } from "../../actions/profile";
+
+
+const Profile = props => {
+    return (
+        <div>
+            this is user's profile
+        </div>
+    )
+}
+
+Profile.propTypes = {
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    profile: state.profile, 
+    auth: state.auth // Need auth state for, if user is looking at profile and if that profile matches their own, then their will be an edit button
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile)
