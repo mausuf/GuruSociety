4be92f2e69abfef4214f5a7342df6a66b414b8a1
starbuck <mausuf@utexas.edu>
2019-07-14 18:57:57 -0500

    Updated remaining social links for ProfileTop component

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -33,22 +33,41 @@ const ProfileTop = ({
               </a>
             )
         }
-
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-twitter fa-2x"></i>
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-facebook fa-2x"></i>
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-linkedin fa-2x"></i>
-          </a>
-           <a href="#" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-youtube fa-2x"></i>
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-instagram fa-2x"></i>
-          </a>
+        {
+            social && social.twitter && (
+                <a href={social.twitter} target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-twitter fa-2x"></i>
+              </a>
+            )
+        }
+        {
+            social && social.facebook && (
+                <a href={social.facebook} target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-facebook fa-2x"></i>
+              </a>
+            )
+        }
+        {
+            social && social.linkedin && (
+                <a href={social.linkedin} target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-linkedin fa-2x"></i>
+              </a> 
+            )
+        }
+        {
+            social && social.youtube && (
+                <a href={social.youtube} target="_blank" rel="noopener noreferrer">
+                <i class="fab fa-youtube fa-2x"></i>
+              </a>
+            )
+        }
+        {
+            social && social.instagram && (
+          <a href={social.instagram} target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-instagram fa-2x"></i>
+        </a>
+            )
+        }
         </div>
       </div>
     )
