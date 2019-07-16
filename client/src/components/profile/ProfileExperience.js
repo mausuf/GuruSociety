b7d878fd876d719b4a5e20d40fa9d71916dd973b
starbuck <mausuf@utexas.edu>
2019-07-15 22:50:38 -0500

    Updated Profile Experience component with experience content and Moment Integration

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -1,14 +1,26 @@
 // intialized with racfp
 import React from 'react'
 import PropTypes from 'prop-types'
+import Moment from 'react-moment';
 
-const ProfileExperience = props => {
-    return (
+const ProfileExperience = ({ experience: {
+    company, title, location, current, to, from, description
+    }
+}) =>
         <div>
-            
+        <h3 className="text-dark">{company}</h3>
+        <p>
+            {/* Check to see if 'To' date exist. - If not to, THEN put Now */}
+            <Moment format='YYYY/MM/DD'>{from}</Moment> - {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{to}</Moment>}
+        </p>
+        <p>
+            <strong>Position: </strong> {title}
+        </p>
+        <p>
+            <strong>Description: </strong> {description}
+        </p>
         </div>
-    )
-}
+
 
 ProfileExperience.propTypes = {
 
